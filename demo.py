9204a422b9be042f1aba1f4dc3d44df82acd4fa8
liujunyuan <ljyuan@bupt.edu.cn>
2021-07-13 11:28:19 +0800

    v0.2

--- demo.py
+++ demo.py
@@ -6,7 +6,6 @@ from processor import Processor
 config = load_config()
 img_list = get_img_list(config["in_path"])
 random.shuffle(img_list)
-num_img = len(img_list)
 processor = Processor(config)
-process_list = processor.get_process_list(num_img)
+process_list = processor.get_process_list(len(img_list))
 processor.process(img_list, process_list)
