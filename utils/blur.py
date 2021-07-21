6d3cffb5885388c8a3a75796e7e68f45215ff713
liujunyuan <ljyuan@bupt.edu.cn>
2021-07-21 10:06:56 +0800

    v0.2

--- utils/blur.py
+++ utils/blur.py
@@ -4,6 +4,8 @@ import random
 
 def add_blur(img):
     k = random.randint(3, 10) * 2 + 1
-    sigmaX = random.randint(1, 24) / 10
+    #TODO: k sigmaX 1to1
+    #sigmaX = random.randint (24 / 10)
+    sigmaX = 2.4
     # print("X", sigmaX)
     return cv2.GaussianBlur(img, (k, k), sigmaX)
