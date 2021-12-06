909d026927d7fef7b2d4d422f83059b17ea10de8
liujunyuan <ljyuan@bupt.edu.cn>
2021-12-06 11:15:40 +0800

    noise level at least sigma1

--- utils/noise.py
+++ utils/noise.py
@@ -5,7 +5,7 @@ import random
 def add_noise(img, noise_level):
     row, col, ch = img.shape
     mean = 0
-    sigma = random.randint(0, noise_level)
+    sigma = random.randint(1, noise_level)
     # print("noise_sigma", sigma)
     # sigma = 3
     gauss = np.random.normal(mean, sigma, (row, col, ch))
