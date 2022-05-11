0bcad27d9fa16538a5e16b98f425e218ecb1c01a
Ch4ndelier <ljyuan@bupt.edu.cn>
2022-05-11 22:00:48 +0800

    notes

--- utils/apply.py
+++ utils/apply.py
@@ -59,6 +59,7 @@ def apply(x, cfg, cfg_total, method):
             down_scale = down_scale_upper_bound
         else:
             down_scale = random.randint(int(down_scale_lower_bound * 20), int(down_scale_upper_bound * 20)) / 20
+        # precision: 0.05
         #down_scale = cfg['fixed_downsample_scale']
         target_size = (h * down_scale, w * down_scale)
         if cfg_total['is_random']:
